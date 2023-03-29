82c221b0c8e635803e0fc4c4f7302e264aee6fef
HIS <hisjosuapro@gmail.com>
2023-03-29 14:29:31 +0200

    Page Présentation BTS SIO

--- vues/presentationbts.php
+++ vues/presentationbts.php
@@ -1,15 +1,51 @@
 <style>
     .BTSSIO{
-        color:saddlebrown;
+        color:chocolate;
         font:bold;
         font-weight: bolder;
+        font-size:xx-large;
+        font-family:Arial, Helvetica, sans-serif;
         text-align: center;
+        background-color: black;
+        margin-left: 25%;
+        margin-right: 25%;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        border-radius: 10px;
     }
-    p{
+    .divp1{
         color:white;
-        font:bold;
-        font-weight: bolder;
         text-align: center;
+        background-color: black;
+        margin-left: 12%;
+        margin-right: 12%;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        border-radius: 10px;
+    }
+    .p1{
+        color:white;
+        text-align: center;
+        background-color: black;
+        margin-bottom: -5px;
+        font-size: medium;
+    }
+    #col {
+        column-count: 2;
+        color: white;
+        background-color: black;
+        text-align: left;
+        margin-left: 3%;
+        margin-right: 3%;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        border-radius: 10px;
+        padding-left: 10px;
+        padding-right: 10px;
+    }
+    .option{
+        text-align: center;
+        color: whitesmoke;
     }
 </style>
 <article>
@@ -20,5 +56,40 @@
     <br>
     <h3 class="BTSSIO">QU'EST CE QUE LE BTS SIO ?</h3>
     <br>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro esse similique dolor quae fuga ducimus earum asperiores sit fugiat doloremque suscipit quibusdam ex, obcaecati inventore dolorum eos quas delectus consequatur?</p>
+    <div class="divp1">
+        <p class="p1">SIO = Services informatiques aux organisations</p>
+        <br>
+        <p>Le Brevet de Technicien Supérieur aux Services Informatiques aux Organisations, s'adresse à ceux qui souhaitent se former en deux ans aux métiers d'administrateur réseau ou de développeur. Pour par la suite intégrer directement le marché du travail ou continuer des études, dans le domaine de l'informatique.</p>
+        <p>Le BTS SIO se compose en 2 option différente : SLAM ou SISR.</p>
+    </div>
+    <br>
+    <div id="col">
+        <h2 class="option">Option SISR</h2>
+        <p>
+        L’acronyme SISR signifie « Solutions d’infrastructure, systèmes et réseaux ».<br>Voici des précisions sur cette formation et ses débouchés.<br>L’option SISR est destinée aux étudiants qui s’orientent vers les métiers liés à la conception et la maintenance d’infrastructures réseaux.<br>Assurer la sécurité, la maintenance et l’installation des réseaux et des équipements informatiques font partie des principales missions des futurs administrateurs, techniciens ou pilotes d’exploitation.<br>Des cours plus généraux viendront compléter la formation et apporter des compétences plus généralistes, permettant ainsi aux diplômés d’être opérationnels dans n’importe quelle entreprise.<br><br>
+Voici les débouchés avec un BTS SIO SISR:<br><br>
+- Technicien de production<br>
+- Technicien d’infrastructure<br>
+- Technicien réseau et télécoms<br>
+- Technicien systèmes et réseaux<br>
+- Administrateur systèmes et réseaux<br>
+- Support systèmes et réseaux<br>
+- Pilote d’exploitation<br>
+- Informaticien support et déploiement
+        </p>
+        <h2 class="option">Option SLAM</h2>
+        <p>
+        Le signe SLAM signifie « Solutions Logicielles et Applications Métier ».<br>Voici quelques indications sur cette seconde option du BTS SIO, ainsi des précisions sur cette formation et ses débouchés.<br>L’option SLAM est destinée aux étudiants qui s’orientent vers les métiers liés à la conception et la maintenance de programmes applicatifs.<br>Grâce à des cours spécifiques, les diplômés seront capables de gérer un parc informatique ou d’administrer un réseau au sein d’une entreprise.<br>Ils pourront également gérer l’intégration, la sécurisation et la configuration des serveurs, mais aussi des postes clients et des équipements d’interconnexion.<br><br>
+Voici les débouchés avec un BTS SIO SLAM:<br><br>
+- Développeur d'applications informatiques<br>
+- Développeur informatique<br>
+- Analyste d'applications ou d'études<br>
+- Analyste programmeur<br>
+- Chargé d'études informatiques<br>
+- Informaticien d'études<br>
+- Programmeur analyste<br>
+- Programmeur d'applications<br>
+- Responsable des services applicatifs<br>
+- Technicien d'études informatiques
+        </p>
 </article>
