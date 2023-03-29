10b88098b5e67d37bc4e0fd071b71744a02df185
HIS <hisjosuapro@gmail.com>
2023-03-29 14:40:13 +0200

    Pearltree veille technologique

--- vues/veille-technologique.php
+++ vues/veille-technologique.php
@@ -1,4 +1,14 @@
+<style>
+iframe {
+display: block;
+margin: 0 auto;
+}
+</style>
+
 <article>
   <br>
   <h1 class="Titrepage">Veille technologique</h1>
+  <br>
+  <br>
+  <iframe  src="https://www.pearltrees.com/thefoxy7800/veille-technologique-ia/id64875001?embed=2&d=202303291437" width="1044" height="783" style="border:0px;" allowtransparency="true"></iframe><span style="display:block; padding-top:2px; color:#818181; font-size:13px;"><a href="https://www.pearltrees.com/thefoxy7800/veille-technologique-ia/id64875001" style=" color:#818181; font-size:13px;" target="_blank">Veille Technologique IA</a>, par <a href="https://www.pearltrees.com/thefoxy7800" style=" color:#818181; font-size:13px;" target="_blank">thefoxy7800</a></span>
 </article>
