e738b113430446185389c03922598e0a92b61630
HIS <hisjosuapro@gmail.com>
2023-04-04 16:02:20 +0200

    Ajout CV Exemple

--- vues/presentation.php
+++ vues/presentation.php
@@ -1,4 +1,14 @@
+<style>
+    .Télécharger{
+        margin-left: 45%;
+        margin-right: 45%;
+    }
+</style>
+
 <article>
     <br>
     <h1 class="Titrepage">Présentation</h1>
+    <br>
+    <br>
+    <input class="Télécharger" type="button" value="Consulter mon CV" onclick="window.location='./Fichier/CV - Josua HIS.pdf';">
 </article>
